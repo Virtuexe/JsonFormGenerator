3487c3aa2c58b721a6dfcf83a52f44ec8304a264
Virtuexe <68084388+Virtuexe@users.noreply.github.com>
2026-01-29 09:27:05 +0100

    cursor class

---
+++ JsonFormGenerator/Cursor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonFormGenerator;
+public class Cursor {
+    private Point pos;
+    private List<int> nextLines = new List<int> { new() };
+    public Cursor Clone() => (Cursor)this.MemberwiseClone();
+    public void Add(Control c, SurveyForm form) {
+        form.Controls.Add(c);
+        c.Location = new(pos.X, pos.Y + form.AutoScrollPosition.Y);
+        pos.X += c.Width;
+        if (nextLines[^1] < c.Height)
+            nextLines[^1] = c.Height;
+    }
+    private int tabCount;
+    public void NextLine() {
+        pos.X = tabCount * 32;
+        pos.Y += nextLines[^1];
+        nextLines.Add(0);
+    }
+    public void Tab() => tabCount++;
+    public void UnTab() => tabCount--;
+    //public void Set(Control c) {
+    //    pos = c.Location;
+    //}
+}
--- JsonFormGenerator/SurveyForm.cs
+++ JsonFormGenerator/SurveyForm.cs
@@ -62,26 +62,3 @@ public partial class SurveyForm : Form {
         c.Dispose();
     }
 }
-public class Cursor {
-    private Point pos;
-    private List<int> nextLines = new List<int> { new() };
-    public Cursor Clone() => (Cursor)this.MemberwiseClone();
-    public void Add(Control c, SurveyForm form) {
-        form.Controls.Add(c);
-        c.Location = new(pos.X, pos.Y + form.AutoScrollPosition.Y);
-        pos.X += c.Width;
-        if (nextLines[^1] < c.Height)
-            nextLines[^1] = c.Height;
-    }
-    private int tabCount;
-    public void NextLine() {
-        pos.X = tabCount * 32;
-        pos.Y += nextLines[^1];
-        nextLines.Add(0);
-    }
-    public void Tab() => tabCount++;
-    public void UnTab() => tabCount--;
-    //public void Set(Control c) {
-    //    pos = c.Location;
-    //}
-}
